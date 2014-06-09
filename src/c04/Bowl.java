0b12b6a37a32e58e54415ac54dab17c13b9f7913
xc145214 <611062541@qq.com>
2014-06-09 18:15:39 +0800

    xc commit

---
+++ src/c04/Bowl.java
@@ -0,0 +1,12 @@
+package c04;
+
+public class Bowl {
+
+	Bowl(int marker) {
+		System.out.println("Bowl(" + marker + ")");
+	}
+
+	void f(int marker) {
+		System.out.println("f(" + marker + ")");
+	}
+}
